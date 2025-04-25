02150dce95c8dc4426a4e428e708f882b583ec30
Le Trang Hanh Do <s3977994@rmit.edu.vn>
2025-04-25 16:23:39 +0700

    Put all the source directories into src/

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const dotenv = require('dotenv');
-const connectDB = require('./config/database');
+const connectDB = require('./src/config/database');
 
 const app = express();
 require('dotenv').config();
