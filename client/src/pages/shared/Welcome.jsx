d1b7e47baf5c89d8da87ed03afade1ca5ed42aa7
Le Trang Hanh Do <s3977994@rmit.edu.vn>
2025-05-17 15:02:02 +0700

    Reduce the height of welcome banner on welcome page

--- client/src/pages/shared/Welcome.jsx
+++ client/src/pages/shared/Welcome.jsx
@@ -22,17 +22,7 @@ export default function Welcome() {
                         </button>
                     </Link>
                 </div>
-                <div className="upper">
-                    <div className="welcome-text">
-                        <h1>WELCOME TO</h1>
-                        <h1 id="web-app-name">planny</h1>
-                        <br />
-                        <p className="slogan">
-                            Where your event gets its moment{" "}
-                        </p>
-                        <p className="slogan">— and its own page.</p>
-                    </div>
-                </div>
+                <HomeCover />
                 <div className="button-container">
                     <Link to="/login-user">
                         <button className="btn-login">LOGIN</button>
